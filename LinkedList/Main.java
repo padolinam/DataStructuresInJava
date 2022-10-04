0aa8cfa03294951880ef6ddd959777ecb851d55c
Monica Padolina <91210534+padolinam@users.noreply.github.com>
2022-10-04 14:34:19 +0800

    Create Main.java

---
+++ LinkedList/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+   public static void main(String[] args){
+      LinkedList myLinkedList = new LinkedList(2);
+      myLinkedList.append(1);
+      myLinkedList.set(1, 20);
+      myLinkedList.printList();
+
+   }
+}
