37ee90d0939003a59fc1a64c11e7c08a0f81729f
Monica Padolina <91210534+padolinam@users.noreply.github.com>
2022-10-18 13:30:41 +0800

    Create Main.java

---
+++ Stack/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+   public static void main(String[] args){
+      Stack myStack = new Stack(4);
+
+      //myStack.printStack();
+      
+      myStack.push(3);
+      myStack.pop();
+      myStack.printStack();
+   }
+}
